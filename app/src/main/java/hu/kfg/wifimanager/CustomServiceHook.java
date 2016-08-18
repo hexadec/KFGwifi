aeff791bcd2bf412eb6c6378eebf79ea99016c23
hexadec <andris.cseh11@gmail.com>
2016-08-18 09:06:56 +0200

    code updates

--- app/src/main/java/hu/kfg/wifimanager/CustomServiceHook.java
+++ app/src/main/java/hu/kfg/wifimanager/CustomServiceHook.java
@@ -20,6 +20,7 @@ public class CustomServiceHook implements IXposedHookLoadPackage {
 			//lpparam.classLoader.loadClass("com.android.server.am.ActivityManagerService");
 			//XposedBridge.log("wait for inject");
 			//SystemClock.sleep(5000);
+	   		if (Build.VERSION.SDK_INT<17) {XposedBridge.log("SDK version lower than 17, quitting..."); return;}
 			XposedBridge.log("injecting now");
 			XAService.inject(lpparam.classLoader.loadClass("com.android.server.am.ActivityManagerService"));
 		}
