2da474a41c1289b69da867119b38d60ae2b29b43
hexadec <andris.cseh11@gmail.com>
2016-08-20 14:59:14 +0200

    xposed module marshmallow support

--- app/src/main/java/hu/kfg/wifimanager/CustomServiceHook.java
+++ app/src/main/java/hu/kfg/wifimanager/CustomServiceHook.java
@@ -8,6 +8,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import android.app.AndroidAppHelper;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkCapabilities;
 import android.net.NetworkRequest;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -48,10 +49,10 @@ public class CustomServiceHook implements IXposedHookLoadPackage {
 			   protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 				   WifiManager manager = (WifiManager) AndroidAppHelper.currentApplication().getSystemService(Context.WIFI_SERVICE);
 				   SystemClock.sleep(500);
-				   XposedBridge.log("isCaptivePortal: "+((int)param.getResult()));
 				   if (manager.isWifiEnabled()) {
 					   WifiInfo wifiInfo = manager.getConnectionInfo();
 					   if (wifiInfo!=null&&wifiInfo.getSSID()!=null&&wifiInfo.getSSID().equals("\"kfg\"")){
+						   XposedBridge.log("isCaptivePortal: "+((int)param.getResult()));
 						   param.setResult(204);
 					   }
 				   }
@@ -76,11 +77,46 @@ public class CustomServiceHook implements IXposedHookLoadPackage {
 			   protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 				   WifiManager manager = (WifiManager) AndroidAppHelper.currentApplication().getSystemService(Context.WIFI_SERVICE);
 				   SystemClock.sleep(500);
-				   XposedBridge.log("networkValidated: "+(param.args[0]));
 				   if (manager.isWifiEnabled()) {
 					   WifiInfo wifiInfo = manager.getConnectionInfo();
 					   if (wifiInfo!=null&&wifiInfo.getSSID()!=null&&wifiInfo.getSSID().equals("\"kfg\"")){
+						   XposedBridge.log("networkValidated: "+(param.args[0]));
 						   param.args[0] = 1;
+					   }
+				   }
+			   }
+		   });
+		   //Tell NetworkAgentInfo this network is validated and has no captive portal
+		   //Probably this and the next one works
+		   XposedHelpers.findAndHookMethod("com.android.server.ConnectivityService", lpparam.classLoader, "updateCapabilities","com.android.server.connectivity.NetworkAgentInfo", NetworkCapabilities.class,new XC_MethodHook() {
+			   @Override
+			   protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				   WifiManager manager = (WifiManager) AndroidAppHelper.currentApplication().getSystemService(Context.WIFI_SERVICE);
+				   SystemClock.sleep(500);
+				   if (manager.isWifiEnabled()) {
+					   WifiInfo wifiInfo = manager.getConnectionInfo();
+					   if (wifiInfo!=null&&wifiInfo.getSSID()!=null&&wifiInfo.getSSID().equals("\"kfg\"")){
+						   XposedBridge.log("portalDetected: "+(XposedHelpers.getBooleanField(param.args[0],"lastCaptivePortalDetected")));
+						   XposedBridge.log("validated: "+(XposedHelpers.getBooleanField(param.args[0],"lastCaptivePortalDetected")));
+						   XposedHelpers.setBooleanField(param.args[0],"lastCaptivePortalDetected",false);
+						   XposedHelpers.setBooleanField(param.args[0],"lastValidated",true);
+					   }
+				   }
+			   }
+		   });
+		   //Tell NetworkAgentInfo this network is has no restrictions
+		   XposedHelpers.findAndHookMethod("android.net.NetworkCapabilities", lpparam.classLoader,"hasCapability", "int",new XC_MethodHook() {
+			   @Override
+			   protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				   WifiManager manager = (WifiManager) AndroidAppHelper.currentApplication().getSystemService(Context.WIFI_SERVICE);
+				   SystemClock.sleep(500);
+				   if (manager.isWifiEnabled()) {
+					   WifiInfo wifiInfo = manager.getConnectionInfo();
+					   if (wifiInfo!=null&&wifiInfo.getSSID()!=null&&wifiInfo.getSSID().equals("\"kfg\"")){
+						   if ((int)param.args[0]==13){
+							   XposedBridge.log("hasCapability: "+(boolean)param.getResult());
+							   param.setResult(true);
+						   }
 					   }
 				   }
 			   }
